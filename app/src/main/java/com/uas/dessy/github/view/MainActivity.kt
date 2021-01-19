b37a4b1d816eda31b6c01a53064d19635059cd1e
unknown <heryantidessy0@gmail.com>
2021-01-19 20:23:57 +0700

    tugas uas

---
+++ app/src/main/java/com/uas/dessy/github/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.uas.dessy.github.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.uas.dessy.github.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
